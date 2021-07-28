e3f923bdc0a8dbb7128dc3f409b5efa06d393b2b
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-07-28 21:41:51 +0530

    Stack using array implementation completed

--- Stack/Main.java
+++ Stack/Main.java
@@ -2,8 +2,13 @@ package Stack;
 
 public class Main {
     public static void main(String[] args) {
-        String str = "(1 + 2)";
-        BalancedExpression ref = new BalancedExpression();
-        System.out.println(ref.isBalanced(str));
+        Stack stack = new Stack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.pop();
+        System.out.println(stack.peek());
+        System.out.println(stack.isEmpty());
+        System.out.println(stack);
     }
 }
