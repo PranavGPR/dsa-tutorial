2fd3afe548891fa0f854ff32cc40f1623f7ca86f
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-07-30 19:14:26 +0530

    Hashmap basics implemented

---
+++ Hashtable/Main.java
@@ -0,0 +1,13 @@
+package Hashtable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Mosh");
+        map.put(2, "Pranav");
+        System.out.println(map);
+    }
+}
