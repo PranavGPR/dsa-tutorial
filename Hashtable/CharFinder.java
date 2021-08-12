0632c6efef87ae6322316a202d12db6f6deecbf9
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-08-12 18:05:29 +0530

    First Repeated char finder implemented

--- Hashtable/CharFinder.java
+++ Hashtable/CharFinder.java
@@ -1,7 +1,9 @@
 package Hashtable;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class CharFinder {
     public char nonRepeatedChar(String str) {
@@ -18,6 +20,19 @@ public class CharFinder {
             if (map.get(ch) == 1) {
                 return ch;
             }
+        }
+
+        return Character.MIN_VALUE;
+    }
+
+    public char firstRepeatedChar(String str) {
+        Set<Character> set = new HashSet<>();
+        var chars = str.toCharArray();
+
+        for (var ch : chars) {
+            if (set.contains(ch))
+                return ch;
+            set.add(ch);
         }
 
         return Character.MIN_VALUE;
--- Hashtable/Main.java
+++ Hashtable/Main.java
@@ -3,7 +3,7 @@ package Hashtable;
 public class Main {
     public static void main(String[] args) {
         CharFinder finder = new CharFinder();
-        var ch = finder.nonRepeatedChar("a green apple");
+        var ch = finder.firstRepeatedChar("a green apple");
         System.out.println(ch);
     }
 }
