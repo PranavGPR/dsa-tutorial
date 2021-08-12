2e2614d3fed896c852a18c42b79862eee01e7709
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-08-12 11:33:02 +0530

    Non-Repeated Char Finder implemented

---
+++ Hashtable/CharFinder.java
@@ -0,0 +1,25 @@
+package Hashtable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CharFinder {
+    public char nonRepeatedChar(String str) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        var chars = str.toCharArray();
+
+        for (var ch : chars) {
+            var count = map.containsKey(ch) ? map.get(ch) : 0;
+            map.put(ch, count + 1);
+        }
+
+        for (var ch : chars) {
+            if (map.get(ch) == 1) {
+                return ch;
+            }
+        }
+
+        return Character.MIN_VALUE;
+    }
+}
--- Hashtable/Main.java
+++ Hashtable/Main.java
@@ -1,13 +1,9 @@
 package Hashtable;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
-        Map<Integer, String> map = new HashMap<>();
-        map.put(1, "Mosh");
-        map.put(2, "Pranav");
-        System.out.println(map);
+        CharFinder finder = new CharFinder();
+        var ch = finder.nonRepeatedChar("a green apple");
+        System.out.println(ch);
     }
 }
