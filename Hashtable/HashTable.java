855537003e7b2d7bb251ef29200a5cfe2edd58b3
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-08-23 11:52:16 +0530

    put method implemented

---
+++ Hashtable/HashTable.java
@@ -0,0 +1,39 @@
+package Hashtable;
+
+import java.util.LinkedList;
+
+public class HashTable {
+    private class Entry {
+        private int key;
+        private String value;
+
+        public Entry(int key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private LinkedList<Entry>[] entries = new LinkedList[5];
+
+    public void put(int key, String value) {
+        var index = hash(key);
+
+        if (entries[index] == null)
+            entries[index] = new LinkedList<>();
+
+        var bucket = entries[index];
+
+        for (var entry : bucket) {
+            if (entry.key == key) {
+                entry.value = value;
+                return;
+            }
+        }
+
+        bucket.addLast(new Entry(key, value));
+    }
+
+    private int hash(int key) {
+        return key % entries.length;
+    }
+}
