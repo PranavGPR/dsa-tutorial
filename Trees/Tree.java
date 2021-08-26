9a35942a00cbeb4523edc20e72c0c382538d1d2e
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-08-26 19:48:08 +0530

    Implemented isBinarySearchTree method

--- Trees/Tree.java
+++ Trees/Tree.java
@@ -161,4 +161,19 @@ public class Tree {
 
         return false;
     }
+
+    public boolean isBinarySearchTree() {
+        return isBinarySearchTree(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private boolean isBinarySearchTree(Node root, int min, int max) {
+        if (root == null)
+            return true;
+
+        if (root.value < min && root.value > max)
+            return false;
+
+        return isBinarySearchTree(root.leftChild, min, root.value - 1)
+                && isBinarySearchTree(root.rightChild, root.value + 1, max);
+    }
 }
