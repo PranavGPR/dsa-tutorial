6bc773cdf494028eefa03733d5d5903de0fedf67
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-09-28 11:04:36 +0530

    insert method implemented

--- Heap/Heap.java
+++ Heap/Heap.java
@@ -1,13 +1,37 @@
 package Heap;
 
 public class Heap {
-    private int[] arr;
+    private int[] items = new int[10];
+    private int size;
 
     public void insert(int value) {
+        if (isFull())
+            throw new IllegalStateException();
 
+        items[size++] = value;
+
+        bubbleUp();
     }
 
-    public void remove() {
+    private void bubbleUp() {
+        var index = size - 1;
+        while (index > 0 && items[index] > items[parent(index)]) {
+            swap(index, parent(index));
+            index = parent(index);
+        }
+    }
 
+    private int parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    private void swap(int first, int second) {
+        var temp = items[first];
+        items[first] = items[second];
+        items[second] = temp;
+    }
+
+    public boolean isFull() {
+        return size == items.length;
     }
 }
--- Heap/Main.java
+++ Heap/Main.java
@@ -2,6 +2,10 @@ package Heap;
 
 public class Main {
     public static void main(String[] args) {
-        Heap heap = new Heap();
+        var heap = new Heap();
+        heap.insert(10);
+        heap.insert(5);
+        heap.insert(17);
+        System.out.println("Done");
     }
 }
