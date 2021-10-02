809a0c647ae22e22b57f1e70ebaafe15793fc5be
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-10-02 16:52:02 +0530

    insert method implemented

---
+++ Trie/Main.java
@@ -0,0 +1,10 @@
+package Trie;
+
+public class Main {
+    public static void main(String[] args) {
+        var trie = new Trie();
+        trie.insert("cat");
+        trie.insert("can");
+        System.out.println("Done");
+    }
+}
---
+++ Trie/Trie.java
@@ -0,0 +1,44 @@
+package Trie;
+
+import java.util.HashMap;
+
+public class Trie {
+    private class Node {
+        private char value;
+        private HashMap<Character, Node> children = new HashMap<>();
+        private boolean isEndOfWord;
+
+        public Node(char value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Value=" + value;
+        }
+
+        public boolean hasChild(char ch) {
+            return children.containsKey(ch);
+        }
+
+        public void addChild(char ch) {
+            children.put(ch, new Node(ch));
+        }
+
+        public Node getChild(char ch) {
+            return children.get(ch);
+        }
+    }
+
+    private Node root = new Node(' ');
+
+    public void insert(String word) {
+        var current = root;
+        for (var ch : word.toCharArray()) {
+            if (!current.hasChild(ch))
+                current.addChild(ch);
+            current = current.getChild(ch);
+        }
+        current.isEndOfWord = true;
+    }
+}
